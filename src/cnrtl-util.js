bcea119594e08f9b2802b6f66aee2a1ef941e5eb
bbecquet <benjamin.becquet@gmail.com>
2021-03-01 00:00:41 +0100

    Fetch multiple async pages

--- src/cnrtl-util.js
+++ src/cnrtl-util.js
@@ -4,17 +4,43 @@ const cheerio = require('cheerio');
 const { noComma } = require('./utils');
 
 function getDef(word) {
-  return request(getCnrtlURL(word)).then(transform);
+  return fetchDefinitions(word).then(transform);
+}
+
+/**
+ * Fetches all definitions of a word, even when different ones are supposed to be fetched asynchronously
+ * @param {string} word the word to look for
+ */
+function fetchDefinitions(word) {
+  return (
+    request(getCnrtlURL(word))
+      .then(html => {
+        const $ = cheerio.load(html);
+        // the number of tabs on the first result page gives the index of requests to be made
+        return $('#vtoolbar li a')
+          .map(index => `${getCnrtlURL(word)}//${index}`)
+          .get();
+      })
+      .then(urls => Promise.all(urls.map(request)))
+      // a full HTML page is returned each time
+      .then(htmls => htmls.map(extractDefinitionPart))
+      .then(defs => defs.join(''))
+  );
 }
 
 function getCnrtlURL(word) {
   return `https://www.cnrtl.fr/definition/${word}`;
 }
 
+function extractDefinitionPart(html) {
+  const $ = cheerio.load(html);
+  return $('#contentbox').html();
+}
+
 function transform(html) {
   const $ = cheerio.load(html);
   transformExamples($);
-  return $('#contentbox').html();
+  return $.html();
 }
 
 function transformExamples($) {
