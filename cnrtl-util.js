dde6d6c6a5868e792ecb479a89a6f566cbd9a7de
bbecquet <benjamin.becquet@gmail.com>
2021-02-28 10:35:35 +0100

    First Prettier run

--- cnrtl-util.js
+++ cnrtl-util.js
@@ -2,18 +2,16 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 
 function getDef(word) {
-    return request(`http://www.cnrtl.fr/definition/${word}`)
-        .then(extract)
-        .then(transform)
+  return request(`http://www.cnrtl.fr/definition/${word}`).then(extract).then(transform);
 }
 
 function extract(html) {
-    const $ = cheerio.load(html);
-    return $('#contentbox').html();
+  const $ = cheerio.load(html);
+  return $('#contentbox').html();
 }
 
 function transform(html) {
-    return html;
+  return html;
 }
 
 exports.getDef = getDef;
--- index.js
+++ index.js
@@ -5,29 +5,31 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/templates');
 
-app.use(function(req, _res, next) {
-    console.log(new Date().toISOString(), req.originalUrl);
-    next();
+app.use(function (req, _res, next) {
+  console.log(new Date().toISOString(), req.originalUrl);
+  next();
 });
 
-app.get('/def/:word', function(req, res, next) {
-    const word = req.params.word;
-    getDef(word)
-        .then(body => {
-            res.render('def', {
-                def: body,
-                word,
-            });
-        })
-        .catch(() => { next(); })
+app.get('/def/:word', function (req, res, next) {
+  const word = req.params.word;
+  getDef(word)
+    .then((body) => {
+      res.render('def', {
+        def: body,
+        word,
+      });
+    })
+    .catch(() => {
+      next();
+    });
 });
 
 app.use(express.static(__dirname));
 
 // Handle 404
-app.use(function(req, res) {
-    res.status(404);
-    res.send('Not found');
+app.use(function (req, res) {
+  res.status(404);
+  res.send('Not found');
 });
 
 const port = process.env.PORT || 3666;
