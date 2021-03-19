1a2719919f360ef5b1bf655354479209356a2efe
bbecquet <benjamin.becquet@gmail.com>
2021-03-19 23:47:43 +0100

    Reorg for portability in personal site

--- defRoutes.js
+++ defRoutes.js
@@ -1,42 +1,44 @@
-const express = require('express')
-const { handlebars } = require('hbs')
 const { inlineSVG } = require('./src/inlineSVG')
-handlebars.registerHelper('inlineSVG', inlineSVG)
-
-handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
-  return arg1 == arg2 ? options.fn(this) : options.inverse(this)
-})
-
 const { getDef, getCnrtlURL } = require('./src/cnrtl-util')
 
-const router = express.Router()
+function getDefRoutes(router, handlebars) {
+  handlebars.registerHelper('inlineSVG', inlineSVG)
 
-router.get('/:word', function (req, res, next) {
-  const word = req.params.word
-  getDef(word)
-    .then(defs => {
+  handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+    return arg1 == arg2 ? options.fn(this) : options.inverse(this)
+  })
+
+  router.get('/:word', function (req, res, next) {
+    const word = req.params.word
+    getDef(word)
+      .then(defs => {
+        res.render('def', {
+          layout: 'tlfi',
+          defs,
+          word,
+          cnrtlLink: getCnrtlURL(word),
+          cookies: req.cookies,
+        })
+      })
+      .catch(error => {
+        console.error(error)
+        next()
+      })
+  })
+
+  router.get('/', function (req, res, next) {
+    const word = req.query['word']
+    if (word) {
+      res.redirect(`/def/${word.trim()}`)
+    } else {
       res.render('def', {
-        defs,
-        word,
-        cnrtlLink: getCnrtlURL(word),
+        layout: 'tlfi',
         cookies: req.cookies,
       })
-    })
-    .catch(error => {
-      console.error(error)
-      next()
-    })
-})
+    }
+  })
 
-router.get('/', function (req, res, next) {
-  const word = req.query['word']
-  if (word) {
-    res.redirect(`/def/${word.trim()}`)
-  } else {
-    res.render('def', {
-      cookies: req.cookies,
-    })
-  }
-})
+  return router
+}
 
-module.exports = router
+module.exports = getDefRoutes
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { handlebars } = require('hbs')
 const cookieParser = require('cookie-parser')
 
 const app = express()
@@ -11,9 +12,9 @@ app.use(function (req, _res, next) {
   next()
 })
 
-const defs = require('./defRoutes')
+const definitionRoutes = require('./defRoutes')
 
-app.use('/def', defs)
+app.use('/def', definitionRoutes(express.Router(), handlebars))
 app.use(express.static(__dirname))
 
 // Handle 404
